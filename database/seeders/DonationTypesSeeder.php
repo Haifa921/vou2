fa3614e928e7279ee393254622e2ffc73564b6dc
laolao <leenallaf98@gmail.com>
2022-03-03 12:15:45 +0200

    seed

--- database/seeders/DonationTypesSeeder.php
+++ database/seeders/DonationTypesSeeder.php
@@ -15,24 +15,29 @@ class DonationTypesSeeder extends Seeder
     public function run()
     {
         DonationType::factory()->create([
+            'id' => '1',
             'name' => 'ذكاة المال',
-            'min_amount'=>'0.00'
+            'min_amount' => '0.00'
         ]);
         DonationType::factory()->create([
+            'id' => '2',
             'name' => 'سلة غذائية',
-            'min_amount'=>'0.00'
+            'min_amount' => '0.00'
         ]);
         DonationType::factory()->create([
+            'id' => '3',
             'name' => 'تدفئة',
-            'min_amount'=>'16000.00'
+            'min_amount' => '16000.00'
         ]);
         DonationType::factory()->create([
+            'id' => '4',
             'name' => 'ملابس',
-            'min_amount'=>'10000.00'
+            'min_amount' => '10000.00'
         ]);
         DonationType::factory()->create([
+            'id' => '5',
             'name' => 'جراجة',
-            'min_amount'=>'0.00'
+            'min_amount' => '0.00'
         ]);
     }
 }
