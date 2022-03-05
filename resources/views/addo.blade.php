89e5893eb170470cd6c745926a5a60087cb492ad
Haifa921 <haifasy7667@gmail.com>
2022-03-05 06:06:19 -0800

    c

--- resources/views/addo.blade.php
+++ resources/views/addo.blade.php
@@ -64,6 +64,46 @@
 	
 	</head>
 	<body>
+	<header id="fh5co-header-section" class="sticky-banner">
+			<div class="container">
+				<div class="nav-header">
+					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
+					<h1 id="fh5co-logo"><a href="index.html">Charity</a></h1>
+					<!-- START #fh5co-menu-wrap -->
+					<nav id="fh5co-menu-wrap" role="navigation">
+						<ul class="sf-menu" id="fh5co-primary-menu">
+							<li class="active">
+								<a href="http://127.0.0.1:8000/">الرئيسية</a>
+							</li>
+                            <li><a href="{{ route('post.about') }}">من نحن</a></li>
+							
+							<li><a href="{{ route('post.contact') }}">تواصل معنا</a></li>
+
+							<li><a class="active" href="{{ route('post.create1') }}">اكفل يتيم</a></li>
+                       
+        @if (Route::has('login'))
+                <li>
+                    @auth
+					
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                       <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">تطوع الان</a></li>
+                      
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"></a>
+                        @endif
+                    @endauth
+							
+						</ul>
+					</nav>
+                  
+            @endif
+				</div>
+			</div>
+	</header>
+		
+       
+
 <div class="container">
     <div class="row">
       <div class="col">
